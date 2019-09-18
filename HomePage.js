7aca9ae91f8d1f1123b37eedc0219412a46eb81a
qiaoj-ka <1491195822@qq.com>
2019-09-18 13:48:05 +0800

    tmall前端第一次提交

---
+++ HomePage.js
@@ -0,0 +1,45 @@
+
+	$(function(){
+	$("div.rightMenu span").mouseenter(function(){
+		var left = $(this).position().left;
+		var top = $(this).position().top;
+		var width = $(this).css("width");
+		var destLeft = parseInt(left) + parseInt(width)/2;
+		$("img#catear").css("left",destLeft);
+		$("img#catear").css("top",top-20);
+		$("img#catear").fadeIn(500);
+	});
+	$("div.rightMenu span").mouseleave(function(){
+		$("img#catear").hide();
+	});
+});
+	function showProductsAsideCategorys(cid){
+	$("div.eachCategory[cid="+cid+"]").css("background-color","white");
+	$("div.eachCategory[cid="+cid+"] a").css("color","#87CEFA");
+	$("div.productsAsideCategorys[cid="+cid+"]").show();
+}
+
+function hideProductsAsideCategorys(cid){
+	$("div.eachCategory[cid="+cid+"]").css("background-color","#e2e2e3");
+	$("div.eachCategory[cid="+cid+"] a").css("color","#000");
+	$("div.productsAsideCategorys[cid="+cid+"]").hide();
+}
+
+$(function(){
+    $("div.eachCategory").mouseenter(function(){
+        var cid = $(this).attr("cid");
+        showProductsAsideCategorys(cid);
+    });
+    $("div.eachCategory").mouseleave(function(){
+        var cid = $(this).attr("cid");
+        hideProductsAsideCategorys(cid);
+    });
+    $("div.productsAsideCategorys").mouseenter(function(){
+    	var cid = $(this).attr("cid");
+    	showProductsAsideCategorys(cid);
+    });
+    $("div.productsAsideCategorys").mouseleave(function(){
+    	var cid = $(this).attr("cid");
+    	hideProductsAsideCategorys(cid);
+    });
+});
